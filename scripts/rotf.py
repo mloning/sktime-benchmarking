fixed random state in rotation forest, min samples issues for pca transform

scripts/rotf.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.base import clone
 from sklearn.decomposition import PCA
 from sklearn.ensemble.forest import ForestClassifier
+from sklearn.ensemble.base import _set_random_states
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils.validation import check_X_y, check_array, check_random_state, check_is_fitted
 from sklearn.exceptions import DataConversionWarning
@@ -39,14 +40,6 @@ class RotationTreeClassifier(DecisionTreeClassifier):
         self.p_sample_subset = p_sample_subset
         self.transformation = transformation
         self.bootstrap_sample_subset = bootstrap_sample_subset
-        self._rng = check_random_state(random_state)
-
-        if self.transformation == "pca":
-            self.transformer = PCA(random_state=random_state)
-        elif self.transformation == "randomized":
-            self.transformer = PCA(svd_solver="randomized", random_state=random_state)
-        else:
-            raise ValueError("`transformation` must be either 'pca' or 'randomized'.")
 
         super(RotationTreeClassifier, self).__init__(
             criterion=criterion,
@@ -61,30 +54,47 @@ class RotationTreeClassifier(DecisionTreeClassifier):
             random_state=random_state,
             presort=presort)
 
+        # set in init
+        self.n_samples_ = None
+        self.n_features_ = None
+        self.classes_ = None
+        self.n_outputs_ = None
+        self._rng = None
+        self.base_transformer_ = None
+
     def transform(self, X, y=None):
         check_is_fitted(self, "rotation_matrix_")
         return np.dot(X, self.rotation_matrix_)
 
-    def _fit_transfomers(self, X, y):
-        self.n_samples_, self.n_features_ = X.shape
-        self.classes_ = np.unique(y)
-        self.n_outputs_ = y.shape[1]
+    def _make_transformer(self, random_state=None):
+        """Make and configure a copy of the `base_transformer` attribute.
+        Warning: This method should be used to properly instantiate new
+        sub-estimators.
+        """
+        transformer = clone(self.base_transformer_)
 
+        if random_state is not None:
+            _set_random_states(transformer, random_state)
+
+        return transformer
+
+    def _fit_transfomers(self, X, y):
         self.rotation_matrix_ = np.zeros((self.n_features_, self.n_features_), dtype=np.float32)
         self.feature_subsets_ = self._random_feature_subsets(min_length=self.min_features_subset,
                                                              max_length=self.max_features_subset)
+
         for i, feature_subset in enumerate(self.feature_subsets_):
-            classes, sample_subset = self._random_sample_subset(y, bootstrap=self.bootstrap_sample_subset)
-            
+            sample_subset = self._random_sample_subset(y, bootstrap=self.bootstrap_sample_subset)
+
             # add more samples if less samples than features in subset
-            if len(sample_subset) < len(feature_subset):
+            while len(sample_subset) < len(feature_subset):
                 n_new_samples = len(feature_subset) - len(sample_subset)
-                _, new_sample_subset = self._random_sample_subset(y, n_samples=n_new_samples)
+                new_sample_subset = self._random_sample_subset(y, n_samples=n_new_samples)
                 sample_subset = np.vstack([sample_subset, new_sample_subset])
 
             n_attempts = 0
             while n_attempts < 10:
-                pca = clone(self.transformer)
+                pca = self._make_transformer(random_state=self.random_state)
 
                 with np.errstate(divide='ignore', invalid='ignore'):
                     pca.fit(X[sample_subset, feature_subset])
@@ -95,37 +105,40 @@ class RotationTreeClassifier(DecisionTreeClassifier):
 
                 if is_na:
                     n_attempts += 1
-                    _, new_sample_subset = self._random_sample_subset(y, n_samples=10)
+                    new_sample_subset = self._random_sample_subset(y, n_samples=10)
                     sample_subset = np.vstack([sample_subset, new_sample_subset])
 
                 else:
                     self.rotation_matrix_[np.ix_(feature_subset, feature_subset)] = pca.components_
                     break
 
-    def _random_sample_subset(self, y, n_samples=None, classes=None, bootstrap=False):
+    def _random_sample_subset(self, y, n_samples=None, bootstrap=False):
         """Select subset of samples (with replacements) conditional on random subset of classes"""
         # get random state object
         rng = self._rng
 
         # get random subset of classes if not given
-        if classes is None:
-            n_classes = rng.randint(1, len(self.classes_) + 1)
-            classes = rng.choice(self.classes_, size=n_classes, replace=False)
+        n_classes = rng.randint(1, len(self.classes_) + 1)
+        classes = rng.choice(self.classes_, size=n_classes, replace=False)
 
         # get samples for selected classes
         isin_classes = np.where(np.isin(y, classes))[0]
+        n_isin_classes = len(isin_classes)
+
+        # set number of samples in subset
+        if n_samples is None:
+            n_samples = np.int(np.ceil(n_isin_classes * self.p_sample_subset))
+        # if n_samples is given, ensure is less than the number of samples in the selected classes
+        else:
+            if n_samples > n_isin_classes:
+                n_samples = n_isin_classes
 
         # randomly select subset of samples for selected classes
-        if n_samples is None:
-            n_samples = int(len(isin_classes) * self.p_sample_subset)
-            if n_samples < 2:
-                n_samples = len(isin_classes)
-
         sample_subset = rng.choice(isin_classes, size=n_samples, replace=bootstrap)
-        return classes, sample_subset[:, None]
+        return sample_subset[:, None]
 
     def _random_feature_subsets(self, min_length, max_length):
-        """Helper function to randomly select subsets of features"""
+        """Randomly select subsets of features"""
         # get random state object
         rng = self._rng
 
@@ -174,8 +187,22 @@ class RotationTreeClassifier(DecisionTreeClassifier):
             # [:, np.newaxis] that does not.
             y = np.reshape(y, (-1, 1))
 
+        if self.transformation == "pca":
+            self.base_transformer_ = PCA()
+        elif self.transformation == "randomized":
+            self.base_transformer_ = PCA(svd_solver="randomized")
+        else:
+            raise ValueError("`transformation` must be either 'pca' or 'randomized'.")
+
+        self.n_samples_, self.n_features_ = X.shape
+        self.classes_ = np.unique(y)
+        self.n_outputs_ = y.shape[1]
+        self._rng = check_random_state(self.random_state)
+
         # fit transfomers
         self._fit_transfomers(X, y)
+
+        # transform data
         Xt = self.transform(X)
 
         # fit estimators on transformed data
@@ -269,3 +296,4 @@ class RotationForestClassifier(ForestClassifier):
         self.min_weight_fraction_leaf = min_weight_fraction_leaf
         self.max_features = max_features
         self.max_leaf_nodes = max_leaf_nodes
+        self.random_state = random_state